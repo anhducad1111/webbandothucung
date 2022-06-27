b8719aac50de42dbdd966c2312a2928becadfa8f
anhducad1111 <anhducad1111@gmail.com>
2022-06-27 23:07:17 +0700

    upload soure code

--- html/script.js
+++ html/script.js
@@ -14,20 +14,20 @@ prev.addEventListener("click",function(){
     index = (index-1 + slide.length) % slide.length
     slide[index].classList.add('active')
 })
-// ---------------------------------navscroll------------------------
-window.addEventListener('scroll', function(){
-    let header = document.querySelector("header");
-    let windowPosition = window.scrollY > 0;
-    header.classList.toggle("scrolling-active", windowPosition);
-})
-// -------------humberber------------
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".header-content-nav");
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("ham-active");
-    navMenu.classList.toggle("ham-active");
-});
-document.querySelector('.header-content-nav-link').forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("ham-active");
-    navMenu.classList.remove("ham-active");
-}));
+// // ---------------------------------navscroll------------------------
+// window.addEventListener('scroll', function(){
+//     let header = document.querySelector("header");
+//     let windowPosition = window.scrollY > 0;
+//     header.classList.toggle("scrolling-active", windowPosition);
+// })
+// // -------------humberber------------
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".header-content-nav");
+// hamburger.addEventListener("click", () => {
+//     hamburger.classList.toggle("ham-active");
+//     navMenu.classList.toggle("ham-active");
+// });
+// document.querySelector('.header-content-nav-link').forEach(n => n.addEventListener("click", () => {
+//     hamburger.classList.remove("ham-active");
+//     navMenu.classList.remove("ham-active");
+// }));
